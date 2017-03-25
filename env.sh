a1420f6c168a62a59840691e60f7cb4cf4e7ce73
Ruben Paz <me@ruben.io>
2017-03-25 17:34:44 +0000

    Environment vars python3

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
-export DOCKER_TLS_VERIFY="1"
-export DOCKER_HOST="tcp://52.232.30.152:2376"
-export DOCKER_CERT_PATH="/home/istar/.docker/machine/machines/dee4"
-export DOCKER_MACHINE_NAME="dee4"
+virtualenv -p /usr/bin/python3 py3env
+source py3env/bin/activate
+
 # Run this command to configure your shell: 
 # eval $(docker-machine env dee4)
