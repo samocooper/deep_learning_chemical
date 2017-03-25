c112d11657257c1a71e8b5dce0c8730b0fc58d07
samcooper <sam.o.cooper@gmail.com>
2017-03-25 23:40:41 +0000

    Updated model to 4 layer NN, consistantly achieves ROC of ~0.86

--- main.py
+++ main.py
@@ -6,52 +6,52 @@ import numpy
 import h5py
 import theano
 import theano.tensor as T
+from sklearn.metrics import roc_auc_score
+
+
+class HiddenLayer(object):
+    def __init__(self, rng, input, n_in, n_out, W=None, b=None):
+        self.input = input
+
+        W_values = numpy.asarray(
+            rng.uniform(
+                low=-numpy.sqrt(6. / (n_in + n_out)),
+                high=numpy.sqrt(6. / (n_in + n_out)),
+                size=(n_in, n_out)
+            ),
+            dtype=theano.config.floatX
+        )
+        b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
+
+        self.W = theano.shared(value=W_values, name='W', borrow=True)
+        self.b = theano.shared(value=b_values, name='b', borrow=True)
+
+        self.Wm = theano.shared(numpy.zeros(W_values.shape, dtype=theano.config.floatX), name='Wm', borrow=True)
+        self.bm = theano.shared(numpy.zeros(b_values.shape, dtype=theano.config.floatX), name='bm', borrow=True)
+
+        lin_output = T.dot(input, self.W) + self.b
+        self.output = (T.nnet.relu(lin_output))
+
+        # parameters of the model
+        self.params = [self.W, self.b]
+        self.m = [self.Wm, self.bm]
 
 
 class LogisticRegression(object):
-    """Multi-class Logistic Regression Class
-
-    The logistic regression is fully described by a weight matrix :math:`W`
-    and bias vector :math:`b`. Classification is done by projecting data
-    points onto a set of hyperplanes, the distance to which is used to
-    determine a class membership probability.
-    """
-
     def __init__(self, input, n_in, n_out):
-        """ Initialize the parameters of the logistic regression
-
-        :type input: theano.tensor.TensorType
-        :param input: symbolic variable that describes the input of the
-                      architecture (one minibatch)
-
-        :type n_in: int
-        :param n_in: number of input units, the dimension of the space in
-                     which the datapoints lie
-
-        :type n_out: int
-        :param n_out: number of output units, the dimension of the space in
-                      which the labels lie
-
-        """
         # start-snippet-1
         # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
-        self.W = theano.shared(
-            value=numpy.zeros(
-                (n_in, n_out),
-                dtype=theano.config.floatX
-            ),
-            name='W',
-            borrow=True
-        )
+
+        W_values = numpy.zeros((n_in, n_out), dtype=theano.config.floatX)
+        self.W = theano.shared(value=W_values, name='W', borrow=True)
+
         # initialize the biases b as a vector of n_out 0s
-        self.b = theano.shared(
-            value=numpy.zeros(
-                (n_out,),
-                dtype=theano.config.floatX
-            ),
-            name='b',
-            borrow=True
-        )
+
+        b_values = numpy.zeros((n_out,), dtype=theano.config.floatX)
+        self.b = theano.shared(value=b_values, name='b', borrow=True)
+
+        self.Wm = theano.shared(numpy.zeros(W_values.shape, dtype=theano.config.floatX), name='Wm', borrow=True)
+        self.bm = theano.shared(numpy.zeros(b_values.shape, dtype=theano.config.floatX), name='bm', borrow=True)
 
         # symbolic expression for computing the matrix of class-membership
         # probabilities
@@ -61,123 +61,64 @@ class LogisticRegression(object):
         # x is a matrix where row-j  represents input training sample-j
         # b is a vector where element-k represent the free parameter of
         # hyperplane-k
-        self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
+        self.y_pred = T.nnet.sigmoid(T.dot(input, self.W) + self.b)
 
         # symbolic description of how to compute prediction as class whose
         # probability is maximal
-        self.y_pred = T.argmax(self.p_y_given_x, axis=1)
         # end-snippet-1
 
         # parameters of the model
         self.params = [self.W, self.b]
+        self.m = [self.Wm, self.bm]
 
         # keep track of model input
         self.input = input
 
-    def negative_log_likelihood(self, y):
-        """Return the mean of the negative log-likelihood of the prediction
-        of this model under a given target distribution.
-
-        .. math::
-
-            \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
-            \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|}
-                \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
-            \ell (\theta=\{W,b\}, \mathcal{D})
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-
-        Note: we use the mean instead of the sum so that
-              the learning rate is less dependent on the batch size
-        """
-        # start-snippet-2
-        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
-        # number of examples (call it n) in the minibatch
-        # T.arange(y.shape[0]) is a symbolic vector which will contain
-        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
-        # Log-Probabilities (call it LP) with one row per example and
-        # one column per class LP[T.arange(y.shape[0]),y] is a vector
-        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
-        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
-        # the mean (across minibatch examples) of the elements in v,
-        # i.e., the mean log-likelihood across the minibatch.
-        return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
-        # end-snippet-2
+    def cross_entropy(self, y):
+        return T.mean(T.nnet.binary_crossentropy(self.y_pred, y).mean())
 
     def errors(self, y):
-        """Return a float representing the number of errors in the minibatch
-        over the total number of examples of the minibatch ; zero one
-        loss over the size of the minibatch
-
-        :type y: theano.tensor.TensorType
-        :param y: corresponds to a vector that gives for each example the
-                  correct label
-        """
-
-        # check if y has same dimension of y_pred
-        if y.ndim != self.y_pred.ndim:
-            raise TypeError(
-                'y should have the same shape as self.y_pred',
-                ('y', y.type, 'y_pred', self.y_pred.type)
-            )
-        # check if y is of the correct datatype
-        if y.dtype.startswith('int'):
-            # the T.neq operator returns a vector of 0s and 1s, where 1
-            # represents a mistake in prediction
-            return T.neq(self.y_pred, y)
-        else:
-            raise NotImplementedError()
+        return T.neq(self.y_pred > 0.5, y)
 
 
 def load_data(dataset):
-
-    data = h5py.File('data_matrix.hdf5','a')
+    data = h5py.File('data_matrix.hdf5', 'a')
 
     data_x = data['x_activity'][...]
     data_y = data['y_activity'][...]
 
-    data_x[data_x < 0] = 0
+    data_test = h5py.File('data_matrix_test.hdf5', 'a')
+
+    data_x_test = data_test['x_activity_test'][...]
+    data_y_test = data_test['y_activity_test'][...]
 
     data_y[data_y < 0] = 0
-    data_y = data_y[:,1]
+    data_x[data_x < 0] = 0
 
+    data_y_test[data_y_test < 0] = 0
+    data_x_test[data_x_test < 0] = 0
 
-    split = round(data_x.shape[0] - (data_x.shape[0]/9))
+    train_set_x = theano.shared(numpy.asarray(data_x, dtype=theano.config.floatX), borrow=True)
+    train_set_y = theano.shared(numpy.asarray(data_y, dtype=theano.config.floatX), borrow=True)
 
-    print(sum(data_y[split:]))
-
-    train_set_x = theano.shared(numpy.asarray(data_x[:split, :],
-                                           dtype=theano.config.floatX),
-                             borrow=True)
-    train_set_y = theano.shared(numpy.asarray(data_y[:split],
-                                           dtype=theano.config.floatX),
-                             borrow=True)
-
-    valid_set_x = theano.shared(numpy.asarray(data_x[split:, :],
-                                              dtype=theano.config.floatX),
-                                borrow=True)
-    valid_set_y = theano.shared(numpy.asarray(data_y[split:],
-                                              dtype=theano.config.floatX),
-                                borrow=True)
-    train_set_y = T.cast(train_set_y, 'int32')
-    valid_set_y = T.cast(valid_set_y, 'int32')
+    valid_set_x = theano.shared(numpy.asarray(data_x_test, dtype=theano.config.floatX), borrow=True)
+    valid_set_y = theano.shared(numpy.asarray(data_y_test, dtype=theano.config.floatX), borrow=True)
 
     return [train_set_x, train_set_y, valid_set_x, valid_set_y, data_x.shape[1]]
 
-def sgd_optimization_mnist(learning_rate= 0.1, n_epochs=100,
-                           dataset='mnist.pkl',
-                           batch_size=250):
 
-    train_set_x, train_set_y, valid_set_x, valid_set_y,n_in = load_data(dataset)
+def sgd_optimization_mnist(learning_rate=5, n_epochs=21, dataset='mnist.pkl', batch_size=200):
+    train_set_x, train_set_y, valid_set_x, valid_set_y, n_in = load_data(dataset)
 
     # compute number of minibatches for training, validation and testing
+
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
     n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] // batch_size
-    ######################
-    # BUILD ACTUAL MODEL #
-    ######################
+
+    ###############
+    # BUILD MODEL #
+    ###############
+
     print('... building the model')
 
     # allocate symbolic variables for the data
@@ -186,15 +127,45 @@ def sgd_optimization_mnist(learning_rate= 0.1, n_epochs=100,
     # generate symbolic variables for input (x and y represent a
     # minibatch)
     x = T.matrix('x')  # data, presented as rasterized images
-    y = T.ivector('y')  # labels, presented as 1D vector of [int] labels
+    y = T.matrix('y')  # labels, presented as 1D vector of [int] labels
 
     # construct the logistic regression class
     # Each MNIST image has size 28*28
-    classifier = LogisticRegression(input=x, n_in=n_in, n_out=2)
+
+    rng = numpy.random.RandomState(1234)
+
+    hiddenLayer1 = HiddenLayer(
+        rng=rng,
+        input=x,
+        n_in=n_in,
+        n_out=200
+    )
+    hiddenLayer2 = HiddenLayer(
+        rng=rng,
+        input=hiddenLayer1.output,
+        n_in=200,
+        n_out=200
+    )
+    hiddenLayer3 = HiddenLayer(
+        rng=rng,
+        input=hiddenLayer2.output,
+        n_in=200,
+        n_out=100
+    )
+    hiddenLayer4 = HiddenLayer(
+        rng=rng,
+        input=hiddenLayer3.output,
+        n_in=100,
+        n_out=50
+    )
+
+    classifier = LogisticRegression(input=hiddenLayer4.output, n_in=50, n_out=7)
+    params = hiddenLayer1.params + hiddenLayer2.params + hiddenLayer3.params + hiddenLayer4.params + classifier.params
+    momentum = hiddenLayer1.m + hiddenLayer2.m + hiddenLayer3.m + hiddenLayer4.m + classifier.m
 
     # the cost we minimize during training is the negative log likelihood of
     # the model in symbolic format
-    cost = classifier.negative_log_likelihood(y)
+    cost = classifier.cross_entropy(y)
 
     # compiling a Theano function that computes the mistakes that are made by
     # the model on a minibatch
@@ -208,19 +179,22 @@ def sgd_optimization_mnist(learning_rate= 0.1, n_epochs=100,
         }
     )
 
-    # compute the gradient of cost with respect to theta = (W,b)
-    g_W = T.grad(cost=cost, wrt=classifier.W)
-    g_b = T.grad(cost=cost, wrt=classifier.b)
+    test_model = theano.function(
+        inputs=[index],
+        outputs=[cost, classifier.y_pred],
+        givens={
+            x: valid_set_x[index * batch_size: (index + 1) * batch_size],
+            y: valid_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
 
-    # start-snippet-3
-    # specify how to update the parameters of the model as a list of
-    # (variable, update expression) pairs.
-    updates = [(classifier.W, classifier.W - learning_rate * g_W),
-               (classifier.b, classifier.b - learning_rate * g_b)]
+    gparams = [T.grad(cost, param) for param in params]
 
-    # compiling a Theano function `train_model` that returns the cost, but in
-    # the same time updates the parameter of the model based on the rules
-    # defined in `updates`
+    updates = [(param_i, param_i - learning_rate * grad_i - moment_i) for param_i, grad_i, moment_i in
+               zip(params, gparams, momentum)] + \
+              [(moment_i, moment_i * 0.8 + learning_rate * grad_i) for moment_i, grad_i in
+               zip(momentum, gparams)]
+
     train_model = theano.function(
         inputs=[index],
         outputs=cost,
@@ -230,100 +204,65 @@ def sgd_optimization_mnist(learning_rate= 0.1, n_epochs=100,
             y: train_set_y[index * batch_size: (index + 1) * batch_size]
         }
     )
-    # end-snippet-3
+
+    ################################
+    # TEST ROC AUC BEFORE TRAINING #
+    ################################
+
+    data_test = h5py.File('data_matrix_test.hdf5', 'a')
+    data_y_test = data_test['y_activity_test'][...]
+    data_y_test[data_y_test < 0] = 0
+
+    pred_y = numpy.zeros(data_y_test.shape)
+    for i in range(n_valid_batches):
+        cost, pred_y_temp = test_model(i)
+        pred_y[i * batch_size: (i + 1) * batch_size, :] = pred_y_temp
+
+    print('ROC AUC IS', roc_auc_score(data_y_test.flatten(), pred_y.flatten()))
 
     ###############
     # TRAIN MODEL #
     ###############
 
     print('... training the model')
+
     # early-stopping parameters
-    patience = 5000  # look as this many examples regardless
-    patience_increase = 2  # wait this much longer when a new best is
-                                  # found
-    improvement_threshold = 0.995  # a relative improvement of this much is
-                                  # considered significant
-    validation_frequency = min(n_train_batches, patience // 2)
-                                  # go through this many
-                                  # minibatche before checking the network
-                                  # on the validation set; in this case we
-                                  # check every epoch
 
-    best_validation_loss = numpy.inf
-    test_score = 0.
-    start_time = timeit.default_timer()
-
+    validation_frequency = 10
     done_looping = False
     epoch = 0
 
-    validation_losses = [validate_model(i)
-                         for i in range(n_valid_batches)]
-    this_validation_loss = numpy.sum(validation_losses)
-
-    print(n_train_batches,
-              this_validation_loss * 1000.
-          )
-
     while (epoch < n_epochs) and (not done_looping):
-        epoch = epoch + 1
-        train_cost = 0
-        for minibatch_index in range(n_train_batches):
 
+        epoch += 1
+        train_cost = 0
+
+        for minibatch_index in range(n_train_batches):
             minibatch_avg_cost = train_model(minibatch_index)
             train_cost += minibatch_avg_cost
 
             # iteration number
             iter = (epoch - 1) * n_train_batches + minibatch_index
 
-            if (iter + 1) % validation_frequency == 0:
-                # compute zero-one loss on validation set
-                validation_losses = [validate_model(i)
-                                     for i in range(n_valid_batches)]
-                this_validation_loss = numpy.sum(validation_losses)
+        if epoch % validation_frequency == 0:
+            # compute zero-one loss on validation set
+            validation_losses = [validate_model(i) for i in range(n_valid_batches)]
+            this_validation_loss = numpy.sum(validation_losses)
 
-                print(train_cost,
-                        n_train_batches,
-                        this_validation_loss * 1000.
-                )
+            print(epoch, train_cost, this_validation_loss)
+        else:
+            print(epoch, train_cost)
 
+    ################
+    # TEST ROC AUC #
+    ################
 
-    end_time = timeit.default_timer()
-    print(
-        (
-            'Optimization complete with best validation score of %f %%,'
-            'with test performance %f %%'
-        )
-        % (best_validation_loss * 100., test_score * 100.)
-    )
-    print('The code run for %d epochs, with %f epochs/sec' % (
-        epoch, 1. * epoch / (end_time - start_time)))
-    print(('The code for file ' +
-           os.path.split(__file__)[1] +
-           ' ran for %.1fs' % ((end_time - start_time))), file=sys.stderr)
+    pred_y = numpy.zeros(data_y_test.shape)
+    for i in range(n_valid_batches):
+        cost, pred_y_temp = test_model(i)
+        pred_y[i * batch_size: (i + 1) * batch_size, :] = pred_y_temp
 
-
-def predict():
-    """
-    An example of how to load a trained model and use it
-    to predict labels.
-    """
-
-    # load the saved model
-
-    # compile a predictor function
-    predict_model = theano.function(
-        inputs=[classifier.input],
-        outputs=classifier.y_pred)
-
-    # We can test it on some examples from test test
-    dataset='mnist.pkl.gz'
-    datasets = load_data(dataset)
-    test_set_x, test_set_y = datasets[2]
-    test_set_x = test_set_x.get_value()
-
-    predicted_values = predict_model(test_set_x[:10])
-    print("Predicted values for the first 10 examples in test set:")
-    print(predicted_values)
+    print('ROC AUC IS', roc_auc_score(data_y_test.flatten(), pred_y.flatten()))
 
 
 if __name__ == '__main__':
